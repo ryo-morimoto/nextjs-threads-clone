9e60017bfc10ca3dd41878460a9e0e662bc0ab8f
ryo-morimoto <ryo.morimoto.dev@gmail.com>
2023-10-06 07:53:10 +0900

    fix: add FormMessage for error messages

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -149,6 +149,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   onChange={(e) => handleImage(e, field.onChange)}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -168,6 +169,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -186,6 +188,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -204,6 +207,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
