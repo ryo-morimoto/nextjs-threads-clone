fb52f21c80c5f4bdd0414f036f8a83cf719f3b09
ryo-morimoto <ryo.morimoto.dev@gmail.com>
2023-10-06 07:55:41 +0900

    fix: set key to list item component

--- components/shrared/Bottombar.tsx
+++ components/shrared/Bottombar.tsx
@@ -15,7 +15,7 @@ function Bottombar() {
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
           return (
-            <div>
+            <div key={link.label}>
               <Link
                 href={link.route}
                 key={link.label}
